3dd88916031a0130fddce2d3d036bb756a65ec1b
Your Name Main <you@example.com>
2023-04-30 09:55:34 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,2 @@
+var course = "Next Level Web Development Course";
+console.log(course);
---
+++ object.ts
@@ -0,0 +1,14 @@
+const user:{
+    readonly company:"Programming hero",
+    name:string,
+    age:number,
+    isMarried:boolean,
+    wife?: string
+}={
+    company: 'Programming hero',
+    name: 'Montu Mia',
+    age: 52,
+    isMarried: true,
+}
+
+// user.company= "Programming hero Bangladesh"
