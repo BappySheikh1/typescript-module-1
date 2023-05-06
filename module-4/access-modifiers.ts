ee678ca9a8a12984773a7eb2bfe3e99b1e4536fd
Your Name Main <you@example.com>
2023-05-06 03:55:00 -0700

    polymorphism added

---
+++ module-4/access-modifiers.ts
@@ -0,0 +1,27 @@
+
+// class BankAccount{
+//     public readonly id:number;
+//     public name:string;
+//     private _balance:number;
+
+//     constructor(id:number,name:string,balance:number){
+//         this.id=id;
+//         this.name=name;
+//         this._balance=balance;
+//     }
+//     getBalance(){
+//         console.log(`my current balance is ${this._balance}`);
+//     }
+//     addDeposit(amount:number){
+//         this._balance=this._balance + amount;
+//     }
+
+// }
+
+// class StudentAccount extends BankAccount{
+
+// }
+
+// const myAccount=new BankAccount (444,"Bappy",200) // instance
+
+// console.log(myAccount);
